50433812df54748f8719a3f7fc7994f20bb58afa
baby-red <251zs03201@student.zen.ac.jp>
2025-11-23 23:28:28 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -16,14 +16,28 @@ assessmentButton.addEventListener(
 
     // 診断結果表示エリアの作成
     resultDivision.innerText = '';
-    const header = document.createElement('h3'); // h3タグの作成
-    header.innerText = '診断結果'; // タグの内側のテキストを設定
-    resultDivision.appendChild(header); // divタグの子要素として追加
+    
+    // headerDivisionの作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    // bodyDivisionの作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p'); // pタグの作成
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName); // 診断結果を作成
     paragraph.innerText = result; // pタグの内側のテキストを設定
-    resultDivision.appendChild(paragraph); // divタグの子要素としてpタグを追加
+    bodyDivision.appendChild(paragraph); // divタグの子要素としてpタグを追加
+
+    // resultDivisionにbootstrapのスタイルを適用する
+    resultDivision.setAttribute('class','card');
+
+    // headerDivisionとbodyDivisionをresultDivisionに差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
     
     // ツイートエリアの作成
     tweetDivision.innerText = '';
